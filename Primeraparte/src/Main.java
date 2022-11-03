a681e2c9eaa5986a13fea3d37c61a889e0d43bf6
carlosariza9604 <117296668+carlosariza9604@users.noreply.github.com>
2022-11-02 19:51:33 -0500

    Add files via upload

---
+++ Primeraparte/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int num1,num2,num3;
+      num1=3;
+      num2=1;
+      num3=6;
+      System.out.println("el resultado de la suma es: "+ Funcion.Suma(num1,num2,num3));
+
+
+    }
+}
